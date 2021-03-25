58cf649617c50e17d7ca9d26f1388ec23057f734
haohaoo2012 <73508026+haohaoo2012@users.noreply.github.com>
2021-03-25 10:07:23 +0700

    Initial commit

---
+++ app/src/main/java/nguyendoduchao/ueh/edu/food/FoodDetails.java
@@ -0,0 +1,14 @@
+package nguyendoduchao.ueh.edu.food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_details);
+    }
+}
---
+++ app/src/main/java/nguyendoduchao/ueh/edu/food/retrofit/ApiInterface.java
@@ -0,0 +1,18 @@
+package nguyendoduchao.ueh.edu.food.retrofit;
+
+import nguyendoduchao.ueh.edu.food.model.FoodData;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface {
+
+    @GET("fooddata.json")
+    Call<List<FoodData>> getAllData();
+
+
+    // lets make our model class of json data.
+
+}
